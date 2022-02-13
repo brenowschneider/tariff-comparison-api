ef11d7ab9261a9fe4679e55a711d2a65257f39df
Breno Wruck Schneider <brenowschneider@gmail.com>
2022-02-13 15:35:25 +0100

    Sorts comparable products by annual costs ascending; adds unit tests for products comparison

--- src/products/services/products-comparison/products-comparison.service.spec.ts
+++ src/products/services/products-comparison/products-comparison.service.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProductsComparisonService } from './products-comparison.service';
+import { BasicElectricityTariff, PackagedTariff } from '../../comparable-products';
+
+const comparableProducts = [new BasicElectricityTariff(), new PackagedTariff()];
 
 describe('ProductsComparisonService', () => {
     let service: ProductsComparisonService;
@@ -9,12 +12,48 @@ describe('ProductsComparisonService', () => {
             providers: [ProductsComparisonService]
         }).compile();
 
-        service = module.get<ProductsComparisonService>(
-            ProductsComparisonService
-        );
+        service = module.get<ProductsComparisonService>(ProductsComparisonService);
     });
 
-    it('should be defined', () => {
-        expect(service).toBeDefined();
+    it('should return Packaged Tariff as first item when consumption is 3500KWh', () => {
+        const result = service.compareProductsBasedOnAnnualConsumption(comparableProducts, 3500);
+        expect(result).toEqual([
+            {
+                tariffName: 'Packaged Tariff',
+                annualCostsInEuros: 800
+            },
+            {
+                tariffName: 'Basic Electricity Tariff',
+                annualCostsInEuros: 830
+            }
+        ]);
+    });
+
+    it('should return Packaged Tariff as first item when consumption is 4500KWh', () => {
+        const result = service.compareProductsBasedOnAnnualConsumption(comparableProducts, 4500);
+        expect(result).toEqual([
+            {
+                tariffName: 'Packaged Tariff',
+                annualCostsInEuros: 950
+            },
+            {
+                tariffName: 'Basic Electricity Tariff',
+                annualCostsInEuros: 1050
+            }
+        ]);
+    });
+
+    it('should return Basic Electricity Tariff as first item when consumption is 6000KWh', () => {
+        const result = service.compareProductsBasedOnAnnualConsumption(comparableProducts, 6000);
+        expect(result).toEqual([
+            {
+                tariffName: 'Basic Electricity Tariff',
+                annualCostsInEuros: 1380
+            },
+            {
+                tariffName: 'Packaged Tariff',
+                annualCostsInEuros: 1400
+            }
+        ]);
     });
 });
--- src/products/services/products-comparison/products-comparison.service.ts
+++ src/products/services/products-comparison/products-comparison.service.ts
@@ -5,9 +5,18 @@ import { ComparableProduct } from '../../types';
 @Injectable()
 export class ProductsComparisonService {
     public compareProductsBasedOnAnnualConsumption(productsToCompare: ComparableProduct[], annualConsumption: number): ProductDto[] {
-        return productsToCompare.map((product) => ({
-            tariffName: product.name,
-            annualCostsInEuros: product.calculateAnnualCostsInEuros(annualConsumption)
-        }));
+        return productsToCompare
+            .map((product) => ({
+                tariffName: product.name,
+                annualCostsInEuros: product.calculateAnnualCostsInEuros(annualConsumption)
+            }))
+            .sort(this.sortProductsByCostsAscending);
+    }
+
+    // private sortProductsByCostsAscending(products: ProductDto[]) {
+    //     return products.sort((product1, product2) => product1.annualCostsInEuros - product2.annualCostsInEuros);
+    // }
+    private sortProductsByCostsAscending(productA: ProductDto, productB: ProductDto) {
+        return productA.annualCostsInEuros - productB.annualCostsInEuros;
     }
 }
